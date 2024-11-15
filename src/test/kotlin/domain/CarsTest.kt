4d1abd6d73b94acfe5de7cb95336301096fc5ca6
leekyuwon <kyuwoon369@gmail.com>
2024-11-15 21:11:45 +0900

    refactor: 불필요한 테스트용 클래스를 제거하고 함수형 인터페이스 활용하라

--- src/test/kotlin/domain/CarsTest.kt
+++ src/test/kotlin/domain/CarsTest.kt
@@ -3,10 +3,6 @@ package domain
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 
-private class FixedMoveDecision(private val shouldMove: Boolean) : MoveDecision {
-    override fun shouldMove(): Boolean = shouldMove
-}
-
 class CarsTest : BehaviorSpec({
 
     Given("여러 대의 자동차가 있고") {
@@ -15,7 +11,7 @@ class CarsTest : BehaviorSpec({
         val car3 = Car()
 
         When("모든 자동차가 전진하도록 결정된 경우") {
-            val cars = Cars(listOf(car1, car2, car3), FixedMoveDecision(true))
+            val cars = Cars(listOf(car1, car2, car3)) { true }
             cars.race()
 
             Then("모든 자동차는 한 칸 전진한다") {
@@ -24,7 +20,7 @@ class CarsTest : BehaviorSpec({
         }
 
         When("모든 자동차가 멈추도록 결정된 경우") {
-            val cars = Cars(listOf(car1, car2, car3), FixedMoveDecision(false))
+            val cars = Cars(listOf(car1, car2, car3)) { false }
             cars.race()
 
             Then("모든 자동차는 위치를 유지한다") {

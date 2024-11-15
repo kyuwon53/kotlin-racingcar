304548f44ece7591a98d4ab486e17c49e68456f1
leekyuwon <kyuwoon369@gmail.com>
2024-11-15 22:50:52 +0900

    refactor: RaceGame에서 자동차 수 대신 이름 리스트를 입력받도록 변경

--- src/main/kotlin/app/Main.kt
+++ src/main/kotlin/app/Main.kt
@@ -6,11 +6,11 @@ import view.InputView
 import view.ResultView
 
 fun main() {
-    val carCount = InputView.getCarCount()
+    val carNames = InputView.getCarNames()
     val rounds = InputView.getRounds()
     val moveDecision = RandomMoveDecision() // 실제 게임에서는 무작위 결정 사용
 
-    val raceGame = RaceGame(carCount, rounds, moveDecision)
+    val raceGame = RaceGame(carNames, rounds, moveDecision)
 
     ResultView.displayStartMessage()
     raceGame.start()

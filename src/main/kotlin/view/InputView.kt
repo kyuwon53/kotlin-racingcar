bdb0b7d3209a58d14b2ea5771b44fc90c92fc42c
leekyuwon <kyuwoon369@gmail.com>
2024-11-15 20:56:01 +0900

    refactor: 가독성을 고려하여 조건문으로 리팩터링하라

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -4,20 +4,22 @@ object InputView {
     fun getCarCount(): Int {
         println("자동차 대수를 입력해주세요:")
 
-        return readLine()?.toIntOrNull()?.takeIf { it > 0 }
-            ?: run {
-                println("올바른 숫자를 입력해주세요.")
-                getCarCount()
-            }
+        val input = readln().toIntOrNull()
+        if (input == null || input <= 0) {
+            println("올바른 숫자를 입력해주세요.")
+            return getCarCount()
+        }
+        return input
     }
 
     fun getRounds(): Int {
         println("시도할 회수는 몇 회인가요?")
 
-        return readLine()?.toIntOrNull()?.takeIf { it > 0 }
-            ?: run {
-                println("올바른 숫자를 입력해주세요.")
-                getRounds()
-            }
+        val input = readln().toIntOrNull()
+        if (input == null || input <= 0) {
+            println("올바른 숫자를 입력해주세요.")
+            return getRounds()
+        }
+        return input
     }
 }

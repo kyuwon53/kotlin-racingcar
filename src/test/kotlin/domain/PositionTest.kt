751226e503830960400f4a0eb098fc56a1782c25
leekyuwon <lkw2v@naver.com>
2024-11-19 15:23:12 +0900

    refactor: 테스트 후 테스트 스텁을 초기화하라

--- src/test/kotlin/domain/PositionTest.kt
+++ src/test/kotlin/domain/PositionTest.kt
@@ -5,7 +5,10 @@ import io.kotest.matchers.shouldBe
 class PositionTest : BehaviorSpec({
 
     Given("Position이 초기화된 상태에서") {
-        val position = Position()
+        var position = Position()
+        afterTest {
+            position = Position()
+        }
 
         Then("거리는 0이다") {
             position.distance() shouldBe 0
@@ -23,7 +26,7 @@ class PositionTest : BehaviorSpec({
             position.move(false)
 
             Then("거리는 유지된다") {
-                position.distance() shouldBe 1
+                position.distance() shouldBe 0
             }
         }
 
@@ -32,7 +35,7 @@ class PositionTest : BehaviorSpec({
             position.move(true)
 
             Then("거리는 충족된 횟수만큼 증가한다") {
-                position.distance() shouldBe 3
+                position.distance() shouldBe 2
             }
         }
     }
